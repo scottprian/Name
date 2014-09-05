0074c14f667b540dd1738e1bf8d0ec5610b818ea
Scott Prian <sjprian@yahoo.com>
2014-09-05 22:03:33 +0000

    Fixing else conditions

--- stupid.py
+++ stupid.py
@@ -1,8 +1,8 @@
 p = raw_input ('Enter ScottHub Username: ')
 if p == 'Mike':
   print ('you are gay')
-if p == 'Jeff':
+elif p == 'Jeff':
   print ('keep whistling')
-if p == 'Scott':
+elif p == 'Scott':
   print ('drum drum drum drum')
 else: print ('I do not know you')
