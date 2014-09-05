11bc95a22ce49ea5fe5c6b86eabb0cb19122e074
Scott Prian <sjprian@yahoo.com>
2014-09-05 18:45:12 +0000

    stupid is not working properly yet

---
+++ stupid.py
@@ -0,0 +1,8 @@
+p = raw_input ('Enter ScottHub Username: ')
+if p == 'Mike':
+  print ('you are gay')
+if p == 'Jeff':
+  print ('keep whistling')
+if p == 'Scott':
+  print ('drum drum drum drum')
+else: print ('I do not know you')
