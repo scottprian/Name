e1ea506a5a7a7badaf6721698fb7c75587807f96
Scott Prian <sjprian@yahoo.com>
2014-09-06 19:21:07 +0000

    hello take one

---
+++ hello.py
@@ -0,0 +1,14 @@
+"""fix this:
+for x in range(0,7):
+ delta = daytime.timedelta(days=x+day)
+ new_date = target_date + delta
+print x
+"""
+print 'Today is Monday, July 4, 1994'
+print 'The weather is sunny, with scattered T-storms.'
+
+h = raw_input('say hello..')
+if h == 'hello':
+ print 'hello to you too!'
+else:
+ print 'well fuck off then!'
